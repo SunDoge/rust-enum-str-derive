6d312b872726201f7324daf0bdf785555e7b1ce0
SunDoge <384813529@qq.com>
2019-01-25 15:11:19 +0800

    2018 edition

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,6 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 */
 
-extern crate heck;
 extern crate proc_macro;
 #[macro_use]
 extern crate quote;
